fb6d50c6b543e8f96284d256f9db160c57172ae2
IshakSmajic <ishaksmajicpac@gmail.com>
2022-02-12 23:06:37 +0100

    Initial commit for the middle section

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { Features } from '..';
+import { Feature } from '../../components';
 import './whatGPT3.css';
 const WhatGPT3 = () => {
   return (
-    <div>WhatGPT3</div>
+    <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
+        <div className="gpt3__whatgpt3-feature">
+          <Features />
+        </div>
+        <div className="gpt3__whatgpt3-heading">
+          <h1 className="gradient-text">
+            The possibilities are beyond your imagination
+          </h1>
+          <p>Explore The Library</p>
+        </div>
+        <div className="gpt3__whatgpt3-container">
+          <Feature />
+          <Feature />
+          <Feature />
+        </div>
+    </div>
   )
 }
 
